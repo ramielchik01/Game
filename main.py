c7b4cf008fc55fb2ec980fe9682ca02bc2117676
ramielchik01 <ramiel_0101@icloud.com>
2026-04-11 23:51:08 +0400

    added new tasks

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import random
+import csv
+import json
+import requests 
+from bs4 import BeautifulSoup
 students = {
   'Manjari Ramiel':{
     'name':"Ramiel",
@@ -26,34 +31,35 @@ def FindStudent(data, name):
     return f'We find student {name}'
   else:
     return 'we could not recognize'
-def MeanScore(data, name):
-  mean = data.get(name)
-  if mean and 'grades' in students:
+def MeanScore(data, full_name):
+  student = data.get(full_name)
+  if student and 'grades' in students:
     scores = students['grades']
-    average = sum(scores) / len(scores)
-  else:
-    "we did not find such student"
+    return sum(scores) / len(scores)
+  return "we did not find such student"
 print(students)
 print(FindStudent(students, "Ramiel"))
-print(f'mean score of {'name'}:{MeanScore(students, 'Maqa')} ')
+print(f"mean score of Maqa:{MeanScore(students, 'Salimli Maqa')} ")
 
 # task2
 
-import random
 def TestYourLuck():
   RandomNumber = random.randint(1, 100)
   Tries = 0
+  winning = False
   while Tries < 7:
     UserNumber = int(input("Write Your Number:"))
     Tries += 1
     if UserNumber == RandomNumber:
      print('You get it')
-     break;
+     winning = True
+     break
     elif UserNumber < RandomNumber:
       print('a little bit more, mate')
     else:
       print('a little bit less, dude')
-  print(f'Sorry bro you loose, correct answer was {RandomNumber}')
+  if not winning:
+    print(f'Sorry bro you loose, correct answer was {RandomNumber}')
 TestYourLuck()
 def PlayGames():
   while True:
@@ -62,3 +68,51 @@ def PlayGames():
     if Question == 'n':
       break;
 PlayGames()
+# task 3
+products = [
+  ['product', 'category', ' price', 'data', 'count'],
+  ['laptop', 'technology', 1000, '2025-10-06', 50],
+  ['phone', 'technology', 300, '2024-03-21', 60],
+  ['tabel', 'furniture', 100, '2023-12-19', 322]
+]
+sales = {}
+products[0].append('revenue')
+for row in products[1:]:
+  price = row[2]
+  count = row[4]
+  row.append(price * count)
+for row in products[1:]:
+  category = row[1]
+  revenue = row[5]
+  sales[category] = sales.get(category, 0) + revenue
+bestOne = products[1]
+for row in products[2:]:
+  if row[5] > bestOne[5:]:
+    bestOne = row
+newData = [['category', 'revenue']]
+for category, total in sales.items():
+  newData.append9([category, total])
+with open('newData.csv', 'w', newline='', encoding='utf-8') as file:
+  writer = csv.writer(file)
+  writer.writerows(newData)
+jsonData = []
+all = products[0]
+for row in products[1:]:
+  item=dict(zip(all, row))
+  jsonData.append(item)
+with open('data.json', 'w',encoding='utf-8') as f:
+  json.dump(jsonData, f, indent=4, ensure_ascii=False)
+  
+
+with open('products_updated.csv', 'w', newline='' ,encoding='utf-8') as file:
+  writer = csv.writer(file)
+  writer.writerows(products)
+print('file ready','sales', sales)
+print(f"The best sell one:{bestOne[0]}(income: {bestOne[5]})")
+print("you can see the data in 'products_updated.csv' ")
+# task 4
+url = ' http://books.toscrape.com'
+answer = requests.get(url)
+if answer.status_code == 200:
+  html = answer.text
+soup = BeautifulSoup(html, 'lxml')
